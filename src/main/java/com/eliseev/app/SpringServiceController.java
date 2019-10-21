b9546e37d1575537bb3603b86b13c2baa1bc123d
EliseevAndrei <eliseev.andrei345@mail.ru>
2019-10-21 18:23:56 +0300

    Add slf4j+log4j. Logging to a file and to a console.

--- src/main/java/com/eliseev/app/SpringServiceController.java
+++ src/main/java/com/eliseev/app/SpringServiceController.java
@@ -1,17 +1,30 @@
 package com.eliseev.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/hello")
 public class SpringServiceController {
+    final Logger logger = LoggerFactory.getLogger(SpringServiceController.class);
 
     @GetMapping
     public String getGreeting() {
+        logger.info("User send GET /hello request");
         return "index";
+    }
+
+    @GetMapping("/{name}")
+    public String getGreetingToUser(@PathVariable(name = "name") String name,
+        Model model) {
+        model.addAttribute("name", name);
+        logger.info("User send GET /hello {} request", name);
+        return "user_home";
     }
 
 }
